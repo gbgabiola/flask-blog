b33ab1ca742ff3ec74adb130ca390abcce0054e1
Genesis Gabiola <gbgabiola@gmail.com>
2023-12-22 16:33:40 +0800

    Get articles from DB

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
-from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -20,8 +19,6 @@ app.secret_key = config("SECRET_KEY")
 # Init MySQL
 mysql = MySQL(app)
 
-Articles = Articles()
-
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -32,13 +29,25 @@ def about():
 
 @app.route("/articles")
 def articles():
-    return render_template("articles.html", articles = Articles)
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template("articles.html", articles=articles)
+    else:
+        msg = "No Articles Found"
+        return render_template("articles.html", msg=msg)
+
+    cur.close()
 
 @app.route("/articles/<string:id>/")
 def article(id):
-    return render_template("article.html", id=id)
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+    article = cur.fetchone()
+    return render_template("article.html", article=article)
 
-# Register Form Class
 class RegisterForm(Form):
     name = StringField("Name", [validators.Length(min=1, max=50)])
     email = StringField("Email", [validators.Length(min=6, max=50)])
@@ -49,7 +58,6 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField("Confirm Password")
 
-# User Register
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm(request.form)
@@ -59,35 +67,21 @@ def register():
         username = form.username.data
         password = sha256_crypt.encrypt(str(form.password.data))
 
-        # Create cursor
         cur = mysql.connection.cursor()
-
-        # Execute query
         cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-
-        # Commit to DB
         mysql.connection.commit()
-
-        # Close connection
         cur.close()
-
         flash("You are now registered and can log in", "success")
-
         return redirect(url_for("login"))
     return render_template("register.html", form=form)
 
-# User login
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # Get Form Fields
         username = request.form["username"]
         password_candidate = request.form["password"]
 
-        # Create cursor
         cur = mysql.connection.cursor()
-
-        # Get user by username
         result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
 
         if result > 0:
@@ -106,7 +100,6 @@ def login():
                 error = "Invalid login"
                 return render_template("login.html", error=error)
 
-            # Close connection
             cur.close()
         else:
             error = "Username not found"
@@ -114,7 +107,6 @@ def login():
 
     return render_template("login.html")
 
-# Check if user logged in
 def is_logged_in(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -125,7 +117,6 @@ def is_logged_in(f):
             return redirect(url_for("login"))
     return wrap
 
-# Logout
 @app.route("/logout")
 @is_logged_in
 def logout():
@@ -133,18 +124,25 @@ def logout():
     flash("You are now logged out", "success")
     return redirect(url_for("login"))
 
-# Dashboard
 @app.route("/dashboard")
 @is_logged_in
 def dashboard():
-    return render_template("dashboard.html")
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
 
-# Article Form Class
+    if result > 0:
+        return render_template("dashboard.html", articles=articles)
+    else:
+        msg = "No Articles Found"
+        return render_template("dashboard.html", msg=msg)
+
+    cur.close()
+
 class ArticleForm(Form):
     title = StringField("Title", [validators.Length(min=1, max=200)])
     body  = TextAreaField("Body", [validators.Length(min=30)])
 
-# Add Article
 @app.route("/add_article", methods=["GET", "POST"])
 @is_logged_in
 def add_article():
