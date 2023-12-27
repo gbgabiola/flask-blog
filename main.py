6ecab9340303c81c8067bb41a8684d4496229709
Genesis Gabiola <gbgabiola@gmail.com>
2023-12-28 03:24:00 +0800

    Refactor code and add an execute_query helper method

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, request
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -19,6 +19,21 @@ app.secret_key = config("SECRET_KEY")
 # Init MySQL
 mysql = MySQL(app)
 
+def execute_query(query, values=None, fetch_one=False):
+    cur = mysql.connection.cursor()
+    if values:
+        cur.execute(query, values)
+    else:
+        cur.execute(query)
+
+    if fetch_one:
+        result = cur.fetchone()
+    else:
+        result = cur.fetchall()
+
+    cur.close()
+    return result
+
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -29,23 +44,16 @@ def about():
 
 @app.route("/articles")
 def articles():
-    cur = mysql.connection.cursor()
-    result = cur.execute("SELECT * FROM articles")
-    articles = cur.fetchall()
-
-    if result > 0:
+    articles = execute_query("SELECT * FROM articles")
+    if articles:
         return render_template("articles.html", articles=articles)
     else:
         msg = "No Articles Found"
         return render_template("articles.html", msg=msg)
 
-    cur.close()
-
 @app.route("/articles/<string:id>/")
 def article(id):
-    cur = mysql.connection.cursor()
-    result = cur.execute("SELECT * FROM articles WHERE id = %s", [id])
-    article = cur.fetchone()
+    article = execute_query("SELECT * FROM articles WHERE id = %s", [id], fetch_one=True)
     return render_template("article.html", article=article)
 
 class RegisterForm(Form):
@@ -67,12 +75,11 @@ def register():
         username = form.username.data
         password = sha256_crypt.encrypt(str(form.password.data))
 
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
-        mysql.connection.commit()
-        cur.close()
+        execute_query("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
         flash("You are now registered and can log in", "success")
         return redirect(url_for("login"))
+
     return render_template("register.html", form=form)
 
 @app.route("/login", methods=["GET", "POST"])
@@ -81,28 +88,15 @@ def login():
         username = request.form["username"]
         password_candidate = request.form["password"]
 
-        cur = mysql.connection.cursor()
-        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        user = execute_query("SELECT * FROM users WHERE username = %s", [username], fetch_one=True)
 
-        if result > 0:
-            # Get stored hash
-            data = cur.fetchone()
-            password = data["password"]
-
-            # Compare Passwords
-            if sha256_crypt.verify(password_candidate, password):
-                session["logged_in"] = True
-                session["username"] = username
-
-                flash("You are now logged in", "success")
-                return redirect(url_for("dashboard"))
-            else:
-                error = "Invalid login"
-                return render_template("login.html", error=error)
-
-            cur.close()
+        if user and sha256_crypt.verify(password_candidate, user["password"]):
+            session["logged_in"] = True
+            session["username"] = username
+            flash("You are now logged in", "success")
+            return redirect(url_for("dashboard"))
         else:
-            error = "Username not found"
+            error = "Invalid login"
             return render_template("login.html", error=error)
 
     return render_template("login.html")
@@ -127,17 +121,12 @@ def logout():
 @app.route("/dashboard")
 @is_logged_in
 def dashboard():
-    cur = mysql.connection.cursor()
-    result = cur.execute("SELECT * FROM articles")
-    articles = cur.fetchall()
-
-    if result > 0:
+    articles = execute_query("SELECT * FROM articles")
+    if articles:
         return render_template("dashboard.html", articles=articles)
     else:
         msg = "No Articles Found"
         return render_template("dashboard.html", msg=msg)
-
-    cur.close()
 
 class ArticleForm(Form):
     title = StringField("Title", [validators.Length(min=1, max=200)])
@@ -151,12 +140,11 @@ def add_article():
         title = form.title.data
         body = form.body.data
 
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session["username"]))
-        mysql.connection.commit()
-        cur.close()
+        execute_query("INSERT INTO articles(title, body, author) VALUES(%s, %s, %s)", (title, body, session["username"]))
+
         flash("Article Created", "success")
         return redirect(url_for("dashboard"))
+
     return render_template("add_article.html", form=form)
 
 @app.route("/edit_article/<string:id>", methods=["GET", "POST"])
@@ -172,12 +160,11 @@ def edit_article(id):
         title = request.form["title"]
         body = request.form["body"]
 
-        cur = mysql.connection.cursor()
-        cur.execute("UPDATE articles SET title = %s, body = %s WHERE id = %s", (title, body, id))
-        mysql.connection.commit()
-        cur.close()
+        execute_query("UPDATE articles SET title = %s, body = %s WHERE id = %s", (title, body, id))
+
         flash("Article Updated", "success")
         return redirect(url_for("dashboard"))
+
     return render_template("edit_article.html", form=form)
 
 @app.route("/delete_article/<string:id>", methods=["POST"])
